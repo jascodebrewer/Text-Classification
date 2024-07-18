698276f2bb93a31758a3f9d62a8ebf9e81412544
jascodebrewer <jasofficial1214@gmail.com>
2024-07-18 16:17:01 +0530

    setup updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Jas",
+    author_email="jasofficial@protonmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
